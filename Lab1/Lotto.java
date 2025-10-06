ea7941c28ecc5425b66eb80a7b5cb8a81ccefced
Macuseq <macusek@student.agh.edu.pl>
2025-10-06 17:56:42 +0200

    Zajęcia 1: Lotto (część)

---
+++ Lab1/Lotto.java
@@ -0,0 +1,13 @@
+import java.util.Random;
+public class Lotto {
+	public static void main(String[] args) {
+		Random random = new Random();
+		int Tab[] = new int[6];
+		
+		for(int i = 0; i<6; i++){
+			for(int j = 0; j<6; j++){
+				if (System.out.print(random.nextInt(50)) = Tab[j]) ;
+			}
+		}
+	}
+}
